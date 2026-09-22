4c6f7f94d692ce37e6628fcd11d98cbd26bf1ac9
Juan Sebastian Hernandez Guevara <juanse2007hg@hotmail.com>
2026-09-22 12:01:35 -0500

    feat: Configuracion del proyecto con la base de datos

---
+++ src/test/java/com/tuempresa/bookstore/BookstoreApiApplicationTests.java
@@ -0,0 +1,12 @@
+package com.tuempresa.bookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
